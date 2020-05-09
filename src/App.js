914c662e68ab05d413a45e70ec7e344fa9e178e1
Juan Cruz <jjuancruzrey@gmail.com>
2020-05-08 21:09:15 -0300

    Add Fragment

--- src/App.js
+++ src/App.js
@@ -4,10 +4,10 @@ import Header from './components/header/header.js';
 import TopCardList from './components/topCardsList/top-card-list.js'
 function App() {
   return (
-      <div>
+      <>
         <Header />
         <TopCardList />
-      </div>
+      </>
   );
 }
 
